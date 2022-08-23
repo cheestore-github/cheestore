781763328943d1b182bb12cae89ca579b156f64d
cheestore493 <cheestore493@gmail.com>
2022-08-23 13:22:19 +0430

    first commit

---
+++ apps/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='accounts'
+urlpatterns = [
+    path('register/', views.RegisterUserView.as_view(), name='register'),
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+]
